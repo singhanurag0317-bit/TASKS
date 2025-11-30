9dbb0ac15910878f6256e17abef16f8e69918750
Anurag <anuragjadon0317@gmail.com>
2025-11-30 11:20:15 +0530

    new tasks

---
+++ ASSIGNMENT-TASKS/Digital Clock/script.js
@@ -0,0 +1,12 @@
+let hrs = document.getElementById("hrs");
+let mins = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+
+setInterval(()=>{
+let currenttime= new Date();
+
+hrs.innerHTML = (currenttime.getHours()<10?"0":"") + currenttime.getHours();
+mins.innerHTML =(currenttime.getMinutes()<10?"0":"") +  currenttime.getMinutes();
+sec.innerHTML = (currenttime.getSeconds()<10?"0":"") + currenttime.getSeconds();
+},1000)
